18bad2ff7682dbeeba010fb4c760fa5854398d4b
Yury <33576679+bar2104y@users.noreply.github.com>
2017-11-14 20:33:25 +0300

    Add files via upload

--- 6.pas
+++ 6.pas
@@ -1,41 +1,32 @@
 Program pas;
 var
 str, str1 :string;
-s,c,i,e,n:integer;
+s,c,i,code:integer;
 begin
+str:='';
 s:=0;
-str1:='';
-  write('¬ведите строку: '); readln(str);
-  
-  for i:=1 to length(str) do
+write('¬ведите строку: '); readln(str);
+str:=str+'+';
+
+
+for i:=1 to length(str) do
+begin
+  if str[i] = '+' then
   begin
-    if (str[i]<>'+') and (str[i]<>'-') then
-        str1:=str1+str[i]
-    else
-    begin
-        val(str1,c,e);
-        s:=i+1;
-    end;
-  end;
-  
-  str1:='';
-  
-  for i:=n to length(str) do
+    Val(str1, c, code);
+    s:=s+c;
+    str1:='';
+  end else if str[i] = '-' then
   begin
-    if (str[i]<>'+') and (str[i]<>'-') then
-        str1:=str1+str[i]
-    else
-    begin        
-        if str[i]='+' then
-            s:=s+c
-        else s:=s-c;
-        val(str1,c,e);
-        str1:='';
-    end
+    Val(str1, c, code);
+    s:=s-c;
+    str1:='';
+  end else
+  begin
+    str1:=str1+str[i];
   end;
+end;
   
-  writeln('Loadind...');
-  sleep(1000);
-  writeln('Complete!');
-  writeln(s)
+
+writeln(s)
 end.
